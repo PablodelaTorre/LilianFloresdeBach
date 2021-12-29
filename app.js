10fd568a7a986e04a20e3c55d4a8945b4fc5c2c2
Pablo <delatorre.pablodaniel@gmail.com>
2021-12-29 15:43:12 -0300

    Avances

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+const items = document.getElementById('items')
+const templateCard = document.getElementById('template-card').content
+const fragment = document.createDocumentFragment()
+
 document.addEventListener('DOMContentLoaded', () => {
     fetchData()
 })
@@ -7,8 +11,19 @@ const fetchData = async () => {
     try {
         const res = await fetch('api.json')
         const data = await res.json()
-        console.log(data)
+        //console.log(data)
+        recorrerTarjetas(data)
+        
     } catch (error) {
         console.log(error)
     }
 }  
+
+const recorrerTarjetas = data => {
+    data.forEach(producto => {
+        templateCard.querySelector('h5').textContent = producto.title
+        const clone = templateCard.cloneNode(true)
+        fragment.appendChild(clone)
+    })
+    items.appendChild(fragment)
+}
