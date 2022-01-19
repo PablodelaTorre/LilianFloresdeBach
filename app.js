db43938912db7f31e464b895bc438f03d0ba19a3
Pablo <delatorre.pablodaniel@gmail.com>
2022-01-19 11:26:58 -0300

    avances

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ const addCarrito = e => {    if (e.target.classList.contains('btn-dark')){
     e.stopPropagation()
 }
 
-const setCarrito = objeto => {    const producto = {
+const setCarrito = objeto => { const producto = {
         id: objeto.querySelector('.btn-dark').dataset.id,
         title: objeto.querySelector('h5').textContent,
         precio: objeto.querySelector('p').textContent,
@@ -55,44 +55,77 @@ const setCarrito = objeto => {    const producto = {
     }
 
     carrito[producto.id] = {...producto}
-    recorrerCarrito()
+    pintarCarrito()
 }
 
-const recorrerCarrito = () => {
-    console.log(carrito)
-    object.values(carrito).forEach( producto => {
+const pintarCarrito = () => {
+    items.innerHTML = ''
+    Object.values(carrito).forEach( producto => {
         templateCarrito.querySelector('th').textContent = producto.id
-        templateCarrito.querySelectorAll('td')[0].textContent = producto.cantidad
+        templateCarrito.querySelectorAll('td')[0].textContent = producto.title
+        templateCarrito.querySelectorAll('td')[1].textContent = producto.cantidad
+        templateCarrito.querySelector('.btn-info').dataset.id = producto.id
+        templateCarrito.querySelector('.btn-danger').dataset.id = producto.id
+        templateCarrito.querySelector('span').textContent = producto.cantidad * producto.precio
+        const clone = templateCarrito.cloneNode(true)
+        fragment.appendChild(clone)
+    })
+    items.appendChild(fragment)
+    pintarFooter()
+}
+
+const pintarFooter = () => {
+    footer.innerHTML = ''
+    if (Object.keys(carrito).length === 0){
+        footer.innerHTML = '<th scope="row" colspan="5">Carrito vacío - comience a comprar!</th>'
+        return
+    }
+    const cantidadT = Object.values(carrito).reduce((acc, {cantidad}) => acc + cantidad ,0)
+    const precioT = Object.values(carrito).reduce((acc, {cantidad, precio}) => acc + cantidad * precio,0)
+    templateFooterCarrito.querySelectorAll('td')[0].textContent = cantidadT
+    templateFooterCarrito.querySelector('span').textContent = precioT
+
+    const clone = templateFooterCarrito.cloneNode(true)
+    fragment.appendChild(clone)
+    footer.appendChild(fragment)
+
+    const btnVaciar = document.getElementById('vaciar-carrito')
+    btnVaciar.addEventListener('click', () => {
+        carrito = {}
     })
 }
 
-class TarjetasCarrito {
-    constructor(id, title, precio, cantidad) {
-        this.id = id,
-        this.title = title,
-        this.precio = precio,
-        this.cantidad = cantidad}
-        precioIva(){
-            return (this.precio * 1.21)
-        }
-}
 
-const producto1 = new TarjetasCarrito (1, "Agrimony", 100, 0)
-const producto2 = new TarjetasCarrito (2, "Aspen", 100, 0)
-const producto3 = new TarjetasCarrito (3, "Beech", 120, 0)
-const producto4 = new TarjetasCarrito (4, "Cerato", 150, 0)
 
-console.log("El precio total es de: " + producto1.precioIva() + "$")
 
-let productos = [producto1, producto2, producto3, producto4];
 
-const recorreProductos = (arr) => {
-    for (let i=0; i<=arr.length-1; i++){
-        console.log(productos[i])
-    }    
-}
+// class TarjetasCarrito {
+//     constructor(id, title, precio, cantidad) {
+//         this.id = id,
+//         this.title = title,
+//         this.precio = precio,
+//         this.cantidad = cantidad}
+//         precioIva(){
+//             return (this.precio * 1.21)
+//         }
+// }
 
-recorreProductos(productos)
+// const producto1 = new TarjetasCarrito (1, "Agrimony", 100, 0)
+// const producto2 = new TarjetasCarrito (2, "Aspen", 100, 0)
+// const producto3 = new TarjetasCarrito (3, "Beech", 120, 0)
+// const producto4 = new TarjetasCarrito (4, "Cerato", 150, 0)
+
+// console.log("El precio total es de: " + producto1.precioIva() + "$")
+
+// let productos = [producto1, producto2, producto3, producto4];
+
+// const recorreProductos = (arr) => {
+//     for (let i=0; i<=arr.length-1; i++){
+//         console.log(productos[i])
+//     }    
+// }
+
+// recorreProductos(productos)
 
 
 
