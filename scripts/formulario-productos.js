ad5f18f156e6fb8da82c9689999b410c38c1a477
Pablo <delatorre.pablodaniel@gmail.com>
2022-01-29 00:18:31 -0300

    Proyecto jquery empezado

---
+++ scripts/formulario-productos.js
@@ -0,0 +1,4 @@
+const formProdNombre = $("#form-prod-nombre").val
+const formProdNombre = $("#form-prod-apellido").val
+const formProdNombre = $("#form-prod-email").val
+const formProdNombre = $("#form-prod-text").val
