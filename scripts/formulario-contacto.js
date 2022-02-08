2e7fc75674e36be23f57230625a946b7f61241be
Pablo <delatorre.pablodaniel@gmail.com>
2022-02-08 00:06:06 -0300

    Carrito estilizado

---
+++ scripts/formulario-contacto.js
@@ -0,0 +1,14 @@
+$(document).ready(() => {
+    $("formulario-contacto").on("submit", (e) => {
+        e.preventDefault()
+        const xhttp = new XMLHttpRequest()
+        xhttp.open('GET', '../formulario-contacto.json', true)
+        xhttp.send()
+        
+        xhttp.onreadystatechange = function(){
+            if(this.readyState == 4 && this.status == 200){
+                console.log(this.responseText)
+            }
+        }
+    })
+})
