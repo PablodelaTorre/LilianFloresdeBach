c805ae3340adb3599f68236199b5766d1f0642aa
Pablo <delatorre.pablodaniel@gmail.com>
2022-02-09 17:59:56 -0300

    Casi terminado

--- scripts/formulario-contacto.js
+++ scripts/formulario-contacto.js
@@ -16,21 +16,20 @@ $(document).ready(function() {
         
         xhttp.onreadystatechange = function(){
             if(this.readyState == 4 && this.status == 200){
-                console.log(this.responseText)
+                const errorCorrecto = JSON.parse(this.responseText)
+                console.log(errorCorrecto)
+                let arrEstados =[]
+                for(let i = 0; i < errorCorrecto.length; i++){
+                    arrEstados.push(errorCorrecto[i].estado)
+                }
+                if (nombreContacto != "" && apellidoContacto != "" && emailContacto != "" && textoContacto != ""){
+                    correctoContacto.text = arrEstados[0]
+                    correctoContacto.fadeIn(3000).fadeOut(3000)
+                }else{
+                    errorContacto.text = arrEstados[1]
+                    errorContacto.fadeIn(3000).fadeOut(3000)
+                }
             }
         }
-        // $.ajax({
-        //     method: "POST",
-        //     url: "../formulario-contacto.json",
-        //     data:{
-        //         correcto,
-        //         error,
-        //     },
-        //     success: function (response){
-        //         if (response != "") {
-        //             console.log(response)
-        //         }
-        //     }
-        // })
     })
 })
