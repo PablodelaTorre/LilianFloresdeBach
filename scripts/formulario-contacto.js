0d0aed285cca2d470cab845b007012222f635b0b
Pablo <delatorre.pablodaniel@gmail.com>
2022-02-09 15:13:57 -0300

    Avances

--- scripts/formulario-contacto.js
+++ scripts/formulario-contacto.js
@@ -1,15 +1,30 @@
-$(document).ready(() => {
+$(document).ready(function() {
     const formularioContacto = $("formulario-contacto") 
     formularioContacto.on("submit", (e) => {
         e.preventDefault()
-        const xhttp = new XMLHttpRequest()
-        xhttp.open('GET', '../formulario-contacto.json', true)
-        xhttp.send()
-        
-        xhttp.onreadystatechange = function(){
-            if(this.readyState == 4 && this.status == 200){
-                console.log(this.responseText)
+        $.ajax({
+            method: "POST",
+            url: "../formulario-contacto.json",
+            data:{
+                correcto,
+                error,
+            },
+            success: function (response){
+                if (response != "") {
+                    console.log(response)
+                }
             }
-        }
+        })
+        
+        
+        // const xhttp = new XMLHttpRequest()
+        // xhttp.open('GET', '../formulario-contacto.json', true)
+        // xhttp.send()
+        
+        // xhttp.onreadystatechange = function(){
+        //     if(this.readyState == 4 && this.status == 200){
+        //         console.log(this.responseText)
+        //     }
+        // }
     })
 })
