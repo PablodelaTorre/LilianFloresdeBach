e91640e37f6c2eebdc5e5f77cbc5d91897527bfb
Pablo <delatorre.pablodaniel@gmail.com>
2022-02-09 15:23:15 -0300

    imprime por consola

--- scripts/formulario-contacto.js
+++ scripts/formulario-contacto.js
@@ -1,30 +1,36 @@
 $(document).ready(function() {
-    const formularioContacto = $("formulario-contacto") 
+    const formularioContacto = $("#formulario-contacto") 
+    const nombreContacto = $("#nombre-contacto").val()
+    const apellidoContacto = $("#apellido-contacto").val()
+    const emailContacto = $("#email-contacto").val()
+    const textoContacto = $("#texto-contacto").val()
+
+    const errorContacto = $("#error-contacto")
+    const correctoContacto = $("#correcto-contacto")
+
     formularioContacto.on("submit", (e) => {
         e.preventDefault()
-        $.ajax({
-            method: "POST",
-            url: "../formulario-contacto.json",
-            data:{
-                correcto,
-                error,
-            },
-            success: function (response){
-                if (response != "") {
-                    console.log(response)
-                }
+        const xhttp = new XMLHttpRequest()
+        xhttp.open('GET', '../formulario-contacto.json', true)
+        xhttp.send()
+        
+        xhttp.onreadystatechange = function(){
+            if(this.readyState == 4 && this.status == 200){
+                console.log(this.responseText)
             }
-        })
-        
-        
-        // const xhttp = new XMLHttpRequest()
-        // xhttp.open('GET', '../formulario-contacto.json', true)
-        // xhttp.send()
-        
-        // xhttp.onreadystatechange = function(){
-        //     if(this.readyState == 4 && this.status == 200){
-        //         console.log(this.responseText)
+        }
+        // $.ajax({
+        //     method: "POST",
+        //     url: "../formulario-contacto.json",
+        //     data:{
+        //         correcto,
+        //         error,
+        //     },
+        //     success: function (response){
+        //         if (response != "") {
+        //             console.log(response)
+        //         }
         //     }
-        // }
+        // })
     })
 })
